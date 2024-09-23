3d5c97a611b2d56c19ce3219a0308dcdf1d8d12e
Mariam Magdy <mariaammaagdy00@gmail.com>
2024-09-23 17:30:10 +0300

    UOW and Repositories

--- DataAccessLayer/Entities/MyDbContect.cs
+++ DataAccessLayer/Entities/MyDbContect.cs
@@ -12,33 +12,20 @@ public class MyDbContext : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Gu
     }
    
 
-    // Define DbSets here
+
     public DbSet<User> Users { get; set; }
     public DbSet<Property> Properties { get; set; }
     public DbSet<Payment> Payments { get; set; }
-<<<<<<< HEAD
-    public DbSet<Address> Addresse { get; set; }
+
+    public DbSet<Contract> Contracts { get; set; }
     
-=======
     public DbSet<Address> Addresses { get; set; }
 
->>>>>>> origin/master
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<User>().ToTable("Users");
     }
-<<<<<<< HEAD
- //mariam
- //update
-=======
-<<<<<<< HEAD
- //delete
-=======
-<<<<<<< Updated upstream
-    //commit on ibrahim bransh
-=======
->>>>>>> Stashed changes
->>>>>>> origin/master
->>>>>>> 138ea551c4cff314097d11279c8db4a0f9225dac
+
+
 }
---
+++ DataAccessLayer/Repository/RepositoryBase.cs
@@ -0,0 +1,76 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected MyDbContext Context;
+        public RepositoryBase(MyDbContext _Context)
+        {
+            Context = _Context;
+        }
+        public async Task<IQueryable<T>> GetAllAsync()
+        {
+            var list = await Context.Set<T>().ToListAsync();
+            return list.AsQueryable();
+        }
+
+        public async Task<T> GetByIdAsync(Guid Id)
+        {
+            return await Context.Set<T>().FindAsync(Id);
+        }
+
+        public async Task<T> GetByNameAsync(string name)
+        {
+             return await Context.Set<T>().FindAsync(name);
+            //return await Context.Set<T>().FirstOrDefaultAsync(e => EF.Property<string>(e, "Name") == name);
+        }
+
+        public async Task HardDeleteAsync(Guid Id)
+        {
+            var delete = await GetByIdAsync(Id);
+            Context.Remove(delete);
+            await Context.SaveChangesAsync(); ;
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            await Context.AddAsync(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task SoftDeleteAsync(Guid id)
+        {
+            if (typeof(T) == typeof(Property))
+            {
+                var entity = await Context.Set<Property>().FindAsync(id);
+                if (entity != null)
+                {
+                    entity.IsAvailable = false; 
+                    await SaveChangesAsync();
+                }
+            }
+            else
+            {
+                throw new InvalidOperationException("Soft delete is not supported for this entity type.");
+            }
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            Context.Set<T>().Update(entity);
+            await SaveChangesAsync();
+        }
+    }
+}
