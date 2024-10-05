14df638189d88d5ab4c198bb562a4d516650c070
Ibrahim Ahmed <aebrahem743@gmail.com>
2024-10-05 16:19:42 +0300

    ,.,.

---
+++ PresentationLayer/Models/UserEditViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PresentationLayer.Models
+{
+    public class UserEditViewModel
+    {
+        public Guid Id{ get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public IFormFile UserPicture { get; set; }
+        public string UserPictureURL { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
