9acd286b74293692572dd9cc647391e0745f85da
Mariam Magdy <mariaammaagdy00@gmail.com>
2024-10-11 10:01:49 +0300

    Update the currency to EGP

--- sinessLayer/DTOModels/ContractDTO.cs
+++ sinessLayer/DTOModels/ContractDTO.cs
@@ -25,9 +25,10 @@ namespace BusinessLayer.DTOModels
 
 		// Additional Contract Information
 		[Required]
-		public DateTime StartDate { get; set; }
+		public DateTime StartDate { get; set; } = DateTime.Now;
 
-		public DateTime? EndDate { get; set; }
+
+		public DateTime? EndDate { get; set; } = DateTime.Now.AddMonths(6);
 
         [Required, MaxLength(20)]
 		public string ContractType { get; set; } // Lease, Ownership, etc.
